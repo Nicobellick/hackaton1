4350961165eab1aef7222e7922f84eb56e1f76be
alexandresegault <alexandresegault@gmail.com>
2021-02-18 23:09:54 +0100

    m

--- src/screens/Compatibility.js
+++ src/screens/Compatibility.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Compatibility.css";
 import axios from "axios";
 
 const Compatibility = props => {
+  const [listCharacter, setListCharacter] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://miadil.github.io/starwars-api/api/all.json")
+      .then(res => setListCharacter(res.data.slice(0, 19)))
+      .then(console.log(listCharacter));
+  }, []);
   return (
     <div className="compatiblePage">
       <h2 className="backHomePage">

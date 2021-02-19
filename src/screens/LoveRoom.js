e80dfcefe4eec3961c95954b1129632e38fdb384
RaphNokos <raph.nokos@outlook.com>
2021-02-19 02:08:08 +0100

    game loveRoom

--- src/screens/LoveRoom.js
+++ src/screens/LoveRoom.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./LoveRoom.css";
 
 const LoveRoom = props => {
@@ -7,20 +7,32 @@ const LoveRoom = props => {
   const lover = props.loverChoice;
   const loverImg = props.loverImg;
 
+  const [visibleQ1, setVisibleQ1] = useState(false);
+  const [visibleR1, setVisibleR1] = useState(false);
+  const [visibleR2, setVisibleR2] = useState(false);
+  const [button1, setButton1] = useState("Essaye de conclure")
+ 
   return (
     <div className="loveRoom">
       <h1>Love Room</h1>
-      <button className="buttonStartLove">Essaye de conclure !</button>
+      <button className="buttonStartLove" onClick={() => {
+        setVisibleQ1(!visibleQ1); 
+        setButton1("Introduis-toi !")}}>{button1}</button>
       <div className="charactersLoveurs">
-        <div className="characterLoveur">Ton personnage : {user}</div>
-        <img src={userImg}></img>
-        <div className="starWarsLoveur"> Ton choix : {lover} </div>
-        <img src={loverImg}></img>
+        <div className="characterLoveur">
+            <div className="namePerso">Ton personnage : {user}</div>
+            <img className="imgPerso" src={userImg}/>
+        </div>
+        <div className="characterLoveur">
+            <div className="namePerso"> Ton choix : {lover} </div>
+            <img className="imgPerso"src={loverImg}/>
+        </div>
       </div>
-      <div>
-        <h2>{user}</h2>
-        <img src={userImg}></img>
-      </div>
+        <p className={visibleQ1 ? "questionVisible" : "questionInvisible"} onClick={() => {setVisibleR1(!visibleR1) }}>Salut, moi c'est userName, ça va ? </p>
+        <p className={visibleQ1 ? "questionVisible" : "questionInvisible"} onClick={() => {setVisibleR2(!visibleR2) }}>Est-ce que tu baises ? </p>
+        <p className={visibleR1 ? "reponseVisible" : "reponseInvisible"}> Oui, merci beaucoup de m’avoir choisi !
+        </p>
+        <p className={visibleR2 ? "reponseVisible" : "reponseInvisible"}> Next !</p>
     </div>
   );
 };
